df6e1d6335afc378ea9f4fc6b58638f88d0bada4
YouMadBro!? <amaynar4@my.centennialcollege.ca>
2022-10-28 14:43:59 -0500

    Added comments to each modifed page

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,12 @@
+/*File: /config/config.js
+Student Name: Alexander Maynard
+Student ID: 301170707
+Application Name: COMP229-Midterm-301170707
+Date: 2022-10-28*/
+
+
+//exports login info (username and password) to use MongoDB Atlas Database that is locked with credentials
 export const MongoURI = "mongodb+srv://amaynard2404:helloWorld1243@comp229-midterm-3011707.90csmzu.mongodb.net/comp229_midterm_301170707?retryWrites=true&w=majority";
+
+//exports Secret for use in app.js
 export const Secret = "someSecret";
--- server/controllers/books.js
+++ server/controllers/books.js
@@ -1,3 +1,11 @@
+/*File: controllers/books.js
+Student Name: Alexander Maynard
+Student ID: 301170707
+Application Name: COMP229-Midterm-301170707
+Date: 2022-10-28*/
+
+
+
 // define the book model
 import booksModel from '../models/books.js';
 
@@ -9,6 +17,8 @@ export function displayBookList(req, res, next) {
             console.error(err);
             res.end(err);
         }
+
+        //renders the books/list page with the booksCollection
         res.render('index', { title: 'Book List', page: 'books/list', books: booksCollection });
     });
 }
@@ -16,19 +26,15 @@ export function displayBookList(req, res, next) {
 //  GET the Book Details page in order to add a new Book
 export function displayAddPage(req, res, next) {
 
-    /*****************
-    * ADD CODE HERE *
-    *****************/
+    //renders the books/add page
    res.render('index', { title: 'Add Book', page: 'books/add', book: {} })
 }
 
 // POST process the Book Details page and create a new Book - CREATE
 export function processAddPage(req, res, next) {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+    //creates a new book ready for creation based on what the user enters into the text fields
+    //on the books/add page
     let newBook = booksModel({
         name: req.body.name,
         author: req.body.author,
@@ -37,39 +43,50 @@ export function processAddPage(req, res, next) {
         price: req.body.price
     });
 
+    //create the book to be added to the database
     booksModel.create(newBook, (err, Book) => {
+        
+        //if there's an error this provides the proper message and handling
         if(err){
             console.error(err);
             res.end(err);
         };
 
+        //redirects back to the /books/list page after the book creation
         res.redirect('/books/list');
     })
 }
 
+
 // GET the Book Details page in order to edit an existing Book
 export function displayEditPage(req, res, next) {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-     let id = req.params.id;
+    //assigns id field to the existing one (one we are editing). This id is used to populate the text fields with 
+    //the book's existing content.
+    let id = req.params.id;
 
+
+    //finds a SPECIFIC book (with id) and it's contents from mongodb atlas database for modification
     booksModel.findById(id, (err, Book) => {
+        
+        //if there's an error this provides the proper message and handling
         if(err) {
             console.error(err);
         }
+
+        //renders the books/edit page with content from the book we are looking for
         res.render('index', { title: 'Edit Book', page: 'books/edit', book: Book });
     })
 }
 
 // POST - process the information passed from the details form and update the document
 export function processEditPage(req, res, next) {
-    /*****************
-    * ADD CODE HERE *
-    *****************/
+
+    //assigns id field to the existing one (one we are editing). This id is used to populate the text fields with 
+    //the book's new content (or old content if it is not updated).
    let id = req.params.id;
 
+   //creates a new book to update an existing book based on what the user enters (or already exists) into the text fields. 
    let newBook = booksModel({
         _id: req.body.id,
         name: req.body.name,
@@ -79,29 +96,38 @@ export function processEditPage(req, res, next) {
         price: req.body.price
     });
 
+    //updates an old book based on what the user enters (or already exists) into the text fields. 
+   //the right book is updated through the use of updating by id
     booksModel.updateOne({_id: id}, newBook, (err, Book) => {
+        
+        //if there's an error this provides the proper message and handling
         if(err) {
             console.error(err);
             res.end(err);
         };
     
+        //redirects back to the /books/list page after the book update
         res.redirect('/books/list');
     })
 }
 
+
 // GET - process the delete by user id
 export function processDelete(req, res, next) {
-    /*****************
-  * ADD CODE HERE *
-  *****************/
+
+    //assigns id field to one we want to remove
     let id = req.params.id;
 
+    //deletes the chosen book (by id)
     booksModel.remove({ _id: id }, (err) => {
+        
+        //if there's an error this provides the proper message and handling
         if(err) {
             console.error(err);
             res.end(err);
         }
         
+        //redirects back to the /books/list after the delete
         res.redirect('/books/list');
     })
 }
